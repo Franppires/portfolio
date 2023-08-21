a74fbe2d633c6fa68cf3ebbb2112eabf703ead02
Franciane Pires <franpp22@gmail.com>
2023-08-21 17:05:56 -0300

    Update projects.tsx
    Update

--- src/routes/projects.tsx
+++ src/routes/projects.tsx
@@ -3,13 +3,8 @@ import link from "../assets/link.svg"
 import monica_magalhaes from "../assets/monica_magalhaes.svg"
 import app_pagamentos from "../assets/app_pagamentos.svg"
 import controle_financeiro from "../assets/controle_financeiro.svg"
-import lembrete_aniversarios from "../assets/lembrete_aniversarios.svg"
-import tours from "../assets/tours.svg"
-import mini from "../assets/mini.svg"
 import cadastro from "../assets/cadastro.svg"
 import produto from "../assets/produto.svg"
-import formulario from "../assets/formulario.svg"
-import starbucks from "../assets/starbucks.svg"
 import { Card, CardProject, Title } from '../styles/style'
 import Menu from "./menu"
 import Copyright from "./copyright"
@@ -63,47 +58,6 @@ export default function Projects() {
         </ul>
       </CardProject>
       <CardProject>
-        <img src={lembrete_aniversarios} alt="" />
-        <div>
-          <h3>Lembrete de Aniversários</h3>
-          <a href="https://github.com/Franppires/lembrete_aniversario_front_beginners" target="_blank"><img src={github} alt="" /></a>
-          <a href="https://lembretedeaniversario.netlify.app/" target="_blank"><img src={link} alt="" /></a>
-        </div> 
-
-        <p>Lista de aniversários e componente para redenrizar.</p>
-        <ul>
-          <li>React</li>
-          <li>CSS</li>
-        </ul>
-      </CardProject>
-      <CardProject>
-        <img src={tours} alt="" />
-        <div>
-          <h3>Tours</h3>
-          <a href="https://github.com/Franppires/destinations_tours" target="_blank"><img src={github} alt="" /></a>
-          <a href="https://destinationsandtours.netlify.app/" target="_blank"><img src={link} alt="" /></a>
-        </div>
-        <p>Aplicação que simula o envio de dinheiro para uma outra pessoa, via cartão de crédito usando api. </p>
-        <ul>
-          <li>React</li>
-          <li>CSS</li>
-        </ul>
-      </CardProject>
-      <CardProject>
-        <img src={mini} alt="" />
-        <div>
-          <h3>Controle Financeiro</h3>
-          <a href="https://github.com/Franppires/mini_portfolio_dev_em_dobro" target="_blank"><img src={github} alt="" /></a>
-          <a href="https://franppires.github.io/mini_portfolio_dev_em_dobro/" target="_blank"><img src={link} alt="" /></a>
-        </div>
-        <p>Sistema para cadastrar transação de compra e venda, listagem desses cadastros com total. </p>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Javascript</li>
-        </ul>
-      </CardProject>
-      <CardProject>
           <img src={cadastro} alt="" />
           <div>
             <h3>Cadastro de dados </h3>
@@ -111,48 +65,6 @@ export default function Projects() {
             <a href="https://franppires.github.io/cadastro_dados_newtab/" target="_blank"><img src={link} alt="" /></a>
           </div>
           <p>Cadastro e listagem de novos cadidatos, validação de   formulario e mascara de numero. </p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-          </ul>
-        </CardProject>
-        <CardProject>
-          <img src={formulario} alt="" />
-          <div>
-            <h3>Formulário</h3>
-            <a href="https://github.com/Franppires/form_validation_front_beginner" target="_blank"><img src={github} alt="" /></a>
-            <a href="https://franppires.github.io/form_validation_front_beginner/" target="_blank"><img src={link} alt="" /></a>
-          </div>
-          <p>Validar campos do formulário para preenchimento. </p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-          </ul>
-        </CardProject>
-        <CardProject>
-          <img src={starbucks} alt="" />
-          <div>
-            <h3>Starbucks</h3>
-            <a href="https://github.com/Franppires/starbucks_devclub" target="_blank"><img src={github} alt="" /></a>
-            <a href="https://franppires.github.io/starbucks_devclub/" target="_blank"><img src={link} alt="" /></a>
-          </div>
-          <p>Site clone do starbucks </p>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Javascript</li>
-          </ul>
-        </CardProject>
-        <CardProject>
-          <img src={produto} alt="" />
-          <div>
-            <h3>Visualização de produto </h3>
-            <a href="https://github.com/Franppires/product_preview_frontend_mentor" target="_blank"><img src={github} alt="" /></a>
-            <a href="https://franppires.github.io/product_preview_frontend_mentor/" target="_blank"><img src={link} alt="" /></a>
-          </div>
-          <p>Componente de cartão de visualização do produto </p>
           <ul>
             <li>HTML</li>
             <li>CSS</li>
